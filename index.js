627b26d302f87104998568d3fc7b09501d71c19d
Osgood Gunawan <osgoodgunawan@hotmail.com>
2019-12-26 19:02:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function currentLine(katzDeliLine){
     if(katzDeliLine.length<=0){
      return "The line is currently empty."
     }else{
-      console.log(final)
+      console.log(final);
       return "The line is currently:"+ final
     }
     
